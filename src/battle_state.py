91d15e63c9b8eeee9b338c21a2d6c94540ce12df
nKonnuru <112659605+nKonnuru@users.noreply.github.com>
2025-10-30 01:44:34 -0500

    updated encoding

--- src/battle_state.py
+++ src/battle_state.py
@@ -4,6 +4,10 @@ import poke_env.data.gen_data as gen_data
 
 class battle_state:
     DEX = gen_data.GenData(9)
+    ABILITIES = {0:0, 1:1, 3:'H', 4:'S'}
+    VOLATILE_STATUS = ['none', 'encore', 'taunt', 'confusion']
+    NON_VOLATILE_STATUS = ['none', 'par', 'brn', 'slp', 'frz', 'psn']
+    STAT_STAGES = [0.25, 0.285, 0.33, 0.4, 0.5, 0.66, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
 
     def __init__(self, p1_state, p2_state, global_field_state=torch.zeros(5)):
         """
@@ -17,14 +21,28 @@ class battle_state:
             0: Self Field Conditions (same as global)
                 0: Veil
                 1: Tailwind
+                2: Friend Guard (always active with 2 pokemon)
             1: Pokemon 1
                 0: Name
                 1: Stats
-                2: Volatile Statuses
-                3: Non-volatile Statuses
-                4: Stat Stages
-                5: Moves
-                6: Current HP%
+                    0: HP
+                    1: Atk
+                    2: Def
+                    3: Spa
+                    4: Spd
+                    5: Spe
+                2: Ability Number (refer to ABILITIES)
+                3: Volatile Statuses (refer to VOLATILE_STATUS)
+                4: Non-volatile Statuses (refer to NON_VOLATILE_STATUS)
+                5: Stat Stages
+                    0: Atk
+                    1: Def
+                    2: Spa
+                    3: Spd
+                    4: Spe
+                6: Moves
+                7: Tera [type, active=1 else 0]
+                8: Current HP%
             - Below all same as Pokemon 1
             2: Pokemon 2
             Backup Space:
