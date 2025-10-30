983f854aed252f04a7a54d84ee15d9490f9197f9
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-10-30 17:28:31 -0400

    Fixed dimension battle state

--- src/battle_state.py
+++ src/battle_state.py
@@ -1,59 +1,50 @@
 import torch
 import poke_env.data.gen_data as gen_data
 
+DEX = gen_data.GenData(9)
+VOLATILE_STATUS = ['encore', 'taunt', 'confusion']
+NON_VOLATILE_STATUS = ['none', 'par', 'brn', 'slp', 'frz', 'psn']
+STAT_STAGES = [0.25, 0.285, 0.33, 0.4, 0.5, 0.66, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
 
-# TODO: redesign the state to have two channels each for one player's state
-# mold the global field state into local field states in each players side
 class BattleState:
-    DEX = gen_data.GenData(9)
-    ABILITIES = {0:0, 1:1, 3:'H', 4:'S'}
-    VOLATILE_STATUS = ['none', 'encore', 'taunt', 'confusion']
-    NON_VOLATILE_STATUS = ['none', 'par', 'brn', 'slp', 'frz', 'psn']
-    STAT_STAGES = [0.25, 0.285, 0.33, 0.4, 0.5, 0.66, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0]
+    '''
+    BattleState is a wrapper over a 2x5x22 tensor storing the state of the pokemons
+    of the two players.
 
-    def __init__(self, p1_state, p2_state, global_field_state=torch.zeros(5)):
-        """
-        global_field_state: Tensor for global conditions, all stored as [turns passed] with 0 marking inactive
-            0: Sun
-            1: Rain
-            2: Grassy Terrain
-            3: Psychic Terrain
-            4: Trick Room
-        p1_state: Tensor for player 1's state
-            0: Self Field Conditions (same as global)
-                0: Veil
-                1: Tailwind
-                2: Friend Guard (always active with 2 pokemon)
-            1: Pokemon 1
-                0: Name
-                1: Stats
-                    0: HP
-                    1: Atk
-                    2: Def
-                    3: Spa
-                    4: Spd
-                    5: Spe
-                2: Ability Number (refer to ABILITIES)
-                3: Volatile Statuses (refer to VOLATILE_STATUS)
-                4: Non-volatile Statuses (refer to NON_VOLATILE_STATUS)
-                5: Stat Stages
-                    0: Atk
-                    1: Def
-                    2: Spa
-                    3: Spd
-                    4: Spe
-                6: Moves
-                7: Tera [type, active=1 else 0]
-                8: Current HP%
-            - Below all same as Pokemon 1
-            2: Pokemon 2
-            Backup Space:
-            3: Pokemon 3
-            4: Pokemon 4
-        p2_state: Same as p1_state
-        """
-        self.state = torch.tensor([
-            global_field_state,
-            p1_state,
-            p2_state
-        ])
+    2 channels - one for each player
+
+    5 rows - 1st row for field conditions on respective players side of the field
+                Next 4 rows for the state of each pokemon selected to play
+
+    22 cols for pokemons
+        Col 0 - pokemonID
+        Col 1 - primary typing
+        Col 2 - secondary typing
+        Col 3 - tera burnt or not
+        Col 4 - item held / consumed or knocked off
+        Col 5 - non volatile status condition
+        Col [6-8] - one hot encoding of all volatile status conditions
+        Col 9 - current HP stat
+        Col [10-14] - base stats (excluding HP)
+        Col [15-21] - stat stages (all 6 base stars excluding HP + accuracy and evasion)
+
+    cols for field effects (first 5 are global, last 2 are local, value of 0 means inactive)
+        Col 0 - trick room turns remaining
+        Col 1 - grassy terrain turns remaining
+        Col 2 - psy terrain turns remaining
+        Col 3 - sun turns remaining
+        Col 4 - rain turns remaining
+        Col 5 - tailwind turns remaining
+        Col 6 - aurora veil turns remaining
+        Col [7-21] - padding using 0 (future space to expand ??)
+
+    Additional considerations for the future
+        Store history of moves for moves that are not independent (Protect, Stomping Tantrum)
+        Store Rage Fist stacks for annihilape ??
+    '''
+
+    def __init__(self):
+        # defaults to wrapping over a bunch of zeros
+        return torch.zeros(2,5,22)
+
+    # TODO: maybe this class gets the exact same responsibilities as the RPS dummy state in tests/ ???
