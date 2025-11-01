250dd433e3337bc115bfd2ba8f7f77b9738d0732
GovindButtar <buttargovind@gmail.com>
2025-11-01 09:54:05 -0400

    Update lookups.py

--- src/lookups.py
+++ src/lookups.py
@@ -38,29 +38,37 @@ POKEMON = {
 }
 
 
-NON_VOLATILE_STATUS = ["none", "par", "brn", "slp", "frz", "psn", "tox"]
+NON_VOLATILE_STATUS = {
+    "none" : 0, 
+    "par" : 1,
+    "brn" : 2, 
+    "slp" : 3, 
+    "frz" : 4, 
+    "psn" : 5, 
+    "tox" : 6
+}
 
-TYPES = [
-    "None",
-    "Normal",
-    "Fire",
-    "Water",
-    "Electric",
-    "Grass",
-    "Ice",
-    "Fighting",
-    "Poison",
-    "Ground",
-    "Flying",
-    "Psychic",
-    "Bug",
-    "Rock",
-    "Ghost",
-    "Dragon",
-    "Dark",
-    "Steel",
-    "Fairy",
-]
+TYPES = {
+    "None" : 0,
+    "Normal" : 1,
+    "Fire" : 2,
+    "Water" : 3,
+    "Electric" : 4,
+    "Grass" : 5,
+    "Ice" : 6,
+    "Fighting" : 7,
+    "Poison" : 8,
+    "Ground" : 9,
+    "Flying" : 10,
+    "Psychic" : 11,
+    "Bug" : 12,
+    "Rock" : 13,
+    "Ghost" : 14,
+    "Dragon" : 15,
+    "Dark" : 16,
+    "Steel" : 17,
+    "Fairy" : 18,
+}
 
 def get_base_stat_multiplier(stat_stage):
     if stat_stage >= 0:
