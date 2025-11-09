6808a01ff5a2a70a57b9d7b363cd275ee46901b0
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-11-09 00:10:33 -0500

    Modified pseudo eval to hold the difference in sum of hp of pokemon

--- src/eval.py
+++ src/eval.py
@@ -1,5 +1,9 @@
 from battle_state import BattleState
+import torch
 
 
 def pseudo_eval(state: BattleState) -> float:
-    return 0.0
+    # returns E[sum of health of player's pokemon - sum of health of opponent's pokemon]
+    player_health_sum = torch.sum(state.state[0, 1:5, 9])
+    opponent_health_sum = torch.sum(state.state[1, 1:5, 9])
+    return (player_health_sum - opponent_health_sum).item()
