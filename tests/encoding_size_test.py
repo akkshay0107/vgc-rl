9ee161c69ea10df1cd8d81cb572e323c80be255b
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-11-24 15:01:14 -0500

    Test to check encoding size

---
+++ tests/encoding_size_test.py
@@ -0,0 +1,35 @@
+# Adding src to path
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
+
+from poke_env.battle.pokemon import Pokemon
+from poke_env.teambuilder import TeambuilderPokemon
+
+from encoder import Encoder
+
+if __name__ == "__main__":
+    # Example of pokemon string exceeding 512 tokens
+    pokepaste_string = """
+    Rillaboom @ Miracle Seed
+    Ability: Grassy Surge
+    Level: 50
+    Tera Type: Grass
+    EVs: 244 HP / 196 Atk / 4 Def / 12 SpD / 52 Spe
+    Adamant Nature
+    - Fake Out
+    - Grassy Glide
+    - Wood Hammer
+    - Taunt
+    """
+
+    teambuilder_mon = TeambuilderPokemon.from_showdown(pokepaste_string)
+    pokemon = Pokemon(gen=9, teambuilder=teambuilder_mon)
+
+    pokemon_str = Encoder._get_pokemon_as_text(pokemon, status=2)
+    print(len(pokemon_str))
+    tokens = Encoder.tokenizer.encode(pokemon_str, add_special_tokens=True)
+    print(len(tokens))
+    print("-" * 100)
+    print(pokemon_str)
