f3ac155cb6b0a5b1a84c19be052ec0fa6e330af7
Akkshay Sundara Rajan <sundar57@purdue.edu>
2025-12-27 12:38:13 +0530

    Print available moves for terminal player

--- src/terminal_player.py
+++ src/terminal_player.py
@@ -9,9 +9,57 @@ from poke_env import AccountConfiguration, LocalhostServerConfiguration
 from poke_env.battle import AbstractBattle, DoubleBattle
 from poke_env.player import DefaultBattleOrder, Player, SimpleHeuristicsPlayer
 
-from encoder import Encoder
+from encoder import ACT_SIZE, Encoder
 from env import Gen9VGCEnv
 from teams import RandomTeamFromPool
+
+_TARGET_NAME = {-2: "default", -1: "self", 0: "ally", 1: "opp1", 2: "opp2"}
+
+
+def print_valid_actions_from_mask(battle, action_mask):
+    m = action_mask
+    if torch.is_tensor(m):
+        m = m.detach().cpu().numpy()
+    m = np.asarray(m)
+    if m.ndim == 1:
+        m = m.reshape(2, -1)
+
+    team_list = list(battle.team.values())
+
+    for pos in range(2):
+        active = battle.active_pokemon[pos]
+        if active is None or active.fainted:
+            continue
+
+        mvs = (
+            battle.available_moves[pos]
+            if len(battle.available_moves[pos]) == 1
+            and battle.available_moves[pos][0].id in ["struggle", "recharge"]
+            else list(active.moves.values())
+        )
+
+        print(f"\nPos {pos}: {active.species}")
+
+        if m[pos, 0]:
+            print("0: PASS")
+
+        for i in range(6):
+            a = 1 + i
+            if a < m.shape[1] and m[pos, a]:
+                mon = team_list[i]
+                print(f"{a}: SWITCH -> {mon.species}")
+
+        for a in range(7, ACT_SIZE):
+            if not m[pos, a]:
+                continue
+            gimmick = (a - 7) // 20
+            move_i = ((a - 7) % 20) // 5
+            target = ((a - 7) % 5) - 2
+            if move_i >= len(mvs):
+                continue
+            mv = mvs[move_i]
+            tera = gimmick == 1
+            print(f"{a}: MOVE -> {mv.id} | target={_TARGET_NAME[target]} | tera={tera}")
 
 
 class TerminalPlayer(Player):
@@ -19,22 +67,6 @@ class TerminalPlayer(Player):
         super().__init__(*args, **kwargs)
         self.save_dir = save_dir
         self.episode_data = []
-
-    def _get_action(self, battle: AbstractBattle):
-        obs = self.get_observation(battle).detach().cpu()
-        action_mask = Encoder.get_action_mask(battle).detach().cpu()
-        action = [int(x) for x in input().split()][:2]
-        action_np = np.array(action)
-
-        self.episode_data.append(
-            {
-                "obs": obs,
-                "mask": action_mask,
-                "action": action_np,
-            }
-        )
-
-        return action_np
 
     def _save_episode_data(self):
         save_dir = Path(self.save_dir)
@@ -50,11 +82,22 @@ class TerminalPlayer(Player):
         self.episode_data = []
         return str(save_path)
 
-    def choose_move(self, battle: AbstractBattle):
+    async def choose_move(self, battle: AbstractBattle):
         assert isinstance(battle, DoubleBattle)
         if battle._wait:
             return DefaultBattleOrder()
-        return Gen9VGCEnv.action_to_order(self._get_action(battle), battle)
+
+        obs = self.get_observation(battle).detach().cpu()
+        action_mask = Encoder.get_action_mask(battle).detach().cpu()
+        print_valid_actions_from_mask(battle, action_mask)
+
+        line = await asyncio.to_thread(input, "")
+        action = [int(x) for x in line.split()][:2]
+        action_np = np.asarray(action, dtype=np.int64)
+
+        self.episode_data.append({"obs": obs, "mask": action_mask, "action": action_np})
+
+        return Gen9VGCEnv.action_to_order(action_np, battle)
 
     def get_observation(self, battle: AbstractBattle):
         assert isinstance(battle, DoubleBattle)
