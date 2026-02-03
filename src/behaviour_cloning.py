fcc5108b3f7de41e2b1c2670c8346f83fe44f313
David Oprea <opread012@gmail.com>
2026-02-03 15:07:03 -0500

    Fixed behavior cloning so that the PolicyNet is now used

--- src/behaviour_cloning.py
+++ src/behaviour_cloning.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
+from policy import PolicyNet
 
 class ReplayDataset(Dataset):
     def __init__(self, replays_dir: str):
@@ -24,49 +25,13 @@ class ReplayDataset(Dataset):
 
     def __getitem__(self, idx):
         sample = self.samples[idx]
-
-        # Convert to tensors here and Flatten all tensors to 1D
+        
         return {
-            "obs": sample["obs"].to(torch.float32).reshape(-1),
-            "mask": sample["mask"].to(torch.bool).reshape(-1),
+            "obs": sample["obs"],
+            "mask": sample["mask"].to(torch.bool),
             # converting numpy array action to a tensor
-            "action": torch.tensor(sample["action"], dtype=torch.long).reshape(-1),
+            "action": torch.tensor(sample["action"], dtype=torch.long),
         }
-
-class VGCBehaviorCloningModel(nn.Module):
-    def __init__(self, obs_dim, action_size):
-        super().__init__()
-        action_size = action_size // 2  # Assuming action_size includes both pokemon
-        
-        # Simple feedforward network
-        self.net = nn.Sequential(
-            nn.Linear(obs_dim, 256),
-            nn.ReLU(),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU()
-        )
-
-        # Output heads for action logits
-        self.p1_head = nn.Linear(128, action_size)
-        self.p2_head = nn.Linear(128, action_size)
-        self.log_softmax = nn.LogSoftmax(dim=-1)
-    
-    def forward(self, obs, mask):
-        x = self.net(obs)
-        m1, m2 = mask[:, :47], mask[:, 47:]
-        
-        p1_logits = self.p1_head(x)
-        p2_logits = self.p2_head(x)
-
-        p1_masked_logits = p1_logits.masked_fill(~m1, float("-inf"))
-        p2_masked_logits = p2_logits.masked_fill(~m2, float("-inf"))
-
-        p1_log_probs = self.log_softmax(p1_masked_logits)
-        p2_log_probs = self.log_softmax(p2_masked_logits)
-
-        return p1_log_probs, p2_log_probs
 
 def train_behavior_cloning(replays_path):
     # Configs
@@ -100,18 +65,15 @@ def train_behavior_cloning(replays_path):
 
     # Get dimensions
     sample = dataset[0]
-    obs_dim = sample["obs"].shape[0]
-    num_actions = sample["mask"].shape[-1]
 
     # Initialize model, loss, optimizer
-    model = VGCBehaviorCloningModel(obs_dim, num_actions)
-    criterion = nn.NLLLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)  
+    policy = PolicyNet()
+    optimizer = torch.optim.AdamW(policy.parameters(), lr=LEARNING_RATE, eps=1e-5)  
 
     # Run training loop
     for epoch in range(NUM_EPOCHS):
         print(f"Epoch {epoch + 1}/{NUM_EPOCHS}")
-        model.train()
+        policy.train()
         train_loss = 0.0
         train_correct = 0
         train_total = 0
@@ -123,70 +85,49 @@ def train_behavior_cloning(replays_path):
             target = batch["action"]
             
             optimizer.zero_grad()
-            output = model(obs, mask)  # Assuming single action for simplicity
-
-            # Unpack outputs
-            p1_preds = output[0]
-            p2_preds = output[1]
-
-            p1_correct_moves = target[:, 0]
-            p2_correct_moves = target[:, 1]
-
-            p1_loss = criterion(p1_preds, p1_correct_moves)
-            p2_loss = criterion(p2_preds, p2_correct_moves)
-            loss = p1_loss + p2_loss
+            
+            log_prob, _, _ = policy.evaluate_actions(obs, target, mask)
+            loss = -log_prob.mean()
             loss.backward()
             optimizer.step()
 
             train_loss += loss.item()
 
-            _, p1_preds = torch.max(p1_preds, 1)
-            _, p2_preds = torch.max(p2_preds, 1)
+            with torch.no_grad():
+                logits = policy.get_policy_masked_logits(obs, target, mask)
+                _, preds = torch.max(logits, dim=2)
+                train_total += target.shape[0] * 2 # two pokemon actions
+                train_correct += (preds == target).sum().item()
 
-            train_total += target.shape[0] * 2 # two pokemon actions
-            train_correct += (p1_preds == p1_correct_moves).sum().item()
-            train_correct += (p2_preds == p2_correct_moves).sum().item()
 
         print(f"Training accuracy: {train_correct / train_total}")
 
-        model.eval()
+        policy.eval()
         val_loss = 0.0
         val_correct = 0
         val_total = 0
 
         with torch.no_grad():
             for batch in val_loader:
-                # Get batch data
                 obs = batch["obs"]
                 mask = batch["mask"]
                 target = batch["action"]
-
-                output = model(obs, mask) # Assuming single action for simplicity
-
-                # Unpack outputs
-                p1_preds = output[0]
-                p2_preds = output[1]
-
-                p1_correct_moves = target[:, 0]
-                p2_correct_moves = target[:, 1]
-
-                p1_loss = criterion(p1_preds, p1_correct_moves)
-                p2_loss = criterion(p2_preds, p2_correct_moves)
-                loss = p1_loss + p2_loss
-
+                
+                optimizer.zero_grad()
+                
+                log_prob, _, _ = policy.evaluate_actions(obs, target, mask)
+                loss = -log_prob.mean()
                 val_loss += loss.item()
 
-                _, p1_preds = torch.max(p1_preds, 1)
-                _, p2_preds = torch.max(p2_preds, 1)
-
+                logits = policy.get_policy_masked_logits(obs, target, mask)
+                _, preds = torch.max(logits, dim=2)
                 val_total += target.shape[0] * 2 # two pokemon actions
-                val_correct += (p1_preds == p1_correct_moves).sum().item()
-                val_correct += (p2_preds == p2_correct_moves).sum().item()
-        
+                val_correct += (preds == target).sum().item()
+    
         print(f"Validation accuracy: {val_correct / val_total}")
         print("")
 
-    return model
+    return policy
 
 if __name__ == "__main__":
     PATH_VAR = "C:/Users/oprea/Projects/vgc-rl/replays" # replace with actual path
